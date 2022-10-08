95bdb6f762f79ed72a2b0df17bfe1e78e8881bba
hash-roar <1022647354@qq.com>
2022-10-08 08:10:51 +0000

    :heavy_plus_sign: (fmt and spdlog)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include <fmt/core.h>
-#include <fmt/format.h>
-
 #include <vector>
+#include <spdlog/spdlog.h>
 
-int main(int argc, const char** argv) {
-  std::vector<int> vec{1, 43, 645};
-  fmt::print("{}", vec);
-  return 0;
+int main(int argc, const char** argv)
+{
+
+
+    return 0;
 }
